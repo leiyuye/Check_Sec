e0536315dde8aeeb6721695297f44ff119a2b43f
OldSeven7 <1013244821@qq.com>
2026-05-25 16:43:21 +0800

    部署更新端口和跨域访问

---
+++ src/main/java/com/example/check_sec/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.check_sec;  // 👈 必须和你的启动类包名一样！
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
